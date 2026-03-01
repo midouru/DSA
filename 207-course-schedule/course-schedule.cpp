a10f41721d3db84efe3626e61f518379fcfbaac2
Mridul Tripathi <mridultripathi869@gmail.com>
2026-03-01 11:06:51 +0530

    Time: 1 ms (84.14%) | Memory: 19.4 MB (55.89%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    bool iscycle(int src, vector<vector<int>>& prerequisites, vector<bool>& vis, vector<bool>& recpath){
+    bool iscycle(int src,vector<vector<int>>& adj, vector<vector<int>>& prerequisites, vector<bool>& vis, vector<bool>& recpath){
         vis[src] = true;
         recpath[src] = true;
 
-        for(int i = 0;i<prerequisites.size();i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
+        for(int v : adj[src]){
 
-            if(u == src){
-                if(!vis[v]){
-                    if(iscycle(v,prerequisites,vis,recpath)){
-                        return true;
-                    }
-                }
-                else if(recpath[v]){
+            if(!vis[v]){
+                if(iscycle(v,adj,prerequisites,vis,recpath)){
                     return true;
                 }
             }
+            else if(recpath[v]){
+                return true;
+            }
+            
         }
         recpath[src] = false;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(auto& p:prerequisites){
+            adj[p[0]].push_back(p[1]);
+        }  
         vector<bool> vis(numCourses, false);
         vector<bool> recpath(numCourses, false);
 
         for(int i=0;i<numCourses;i++){
             if(!vis[i]){
-                if(iscycle(i,prerequisites,vis,recpath)){
+                if(iscycle(i,adj,prerequisites,vis,recpath)){
                     return false;
                 }
             }
