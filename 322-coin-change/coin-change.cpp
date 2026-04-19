ba1ff363241514e5f0c0bad2c19655cf0df99ab8
Mridul Tripathi <mridultripathi869@gmail.com>
2026-04-19 21:21:08 +0530

    Time: 19 ms (79.28%) | Memory: 17.9 MB (68.70%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    int getcoincombination(vector<int>& coins, int amount, int i, vector<vector<int>>& dp){
-        if(amount == 0){
-            return 0;
-        }
-
-        if(i>=coins.size() || amount<0){
-            return 1e9;
-        }
-
-        if(dp[i][amount]!= -1){
-            return dp[i][amount];
-        }
-        
-        int take = 1+getcoincombination(coins,amount-coins[i],i,dp);
-        
-        int nottake = getcoincombination(coins,amount,i+1,dp);
-
-        return dp[i][amount] = min(take,nottake);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1,-1));
-        int ans = getcoincombination(coins,amount,0,dp);
-        return (ans >= 1e9) ?  -1 : ans;
+        vector<int> dp(amount+1,1e9);
+        dp[0] = 0;
+        for(int j = 1;j<=amount;j++){
+            for(int coin : coins){
+                if(j-coin >= 0){
+                    dp[j] = min(dp[j],1+dp[j-coin]);
+                }
+            }
+        }
+        return dp[amount] == 1e9 ? -1 : dp[amount];
     }
 };
